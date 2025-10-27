4add445f2996846bd48579f2441d1e6ba084294b
ShubhamKr-047 <shubhamkumar.cse047@gmail.com>
2025-10-28 00:38:48 +0530

    code improvement

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(express.json({ limit: "100mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "100mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+// app.use(bodyParser.json({ limit: "100mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 const allowedOrigins = [
   "http://localhost:3000",
   "https://momento-opal.vercel.app",
